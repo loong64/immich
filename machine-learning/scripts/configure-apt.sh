e1d0bc4d64428a77f58d114f2bbb3fc80cc69407
吴小白 <296015668@qq.com>
2026-08-19 13:47:14 +0800

    feat: add loongarch64 support
    Signed-off-by: 吴小白 <296015668@qq.com>

--- machine-learning/scripts/configure-apt.sh
+++ machine-learning/scripts/configure-apt.sh
@@ -2,8 +2,16 @@
 
 set -e
 
+cat > /etc/apt/sources.list.d/sid.sources << "EOF"
+Types: deb
+URIs: http://deb.debian.org/debian
+Suites: sid
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.pgp
+EOF
+
 sed -i -e's/ main/ main contrib non-free non-free-firmware/g' /etc/apt/sources.list.d/debian.sources
-sed -i -e's/ bookworm-updates/ bookworm-updates sid/g' /etc/apt/sources.list.d/debian.sources
+# sed -i -e's/ bookworm-updates/ bookworm-updates sid/g' /etc/apt/sources.list.d/debian.sources
 
 # default priority is 500, so we set unstable to 450 to prefer stable packages
 cat > /etc/apt/preferences.d/preferences << EOL
